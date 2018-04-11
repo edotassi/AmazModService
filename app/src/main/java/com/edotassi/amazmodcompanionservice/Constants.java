6404bc0282523a99109de3abe13b797ec8d08c66
Edoardo Tassinari <edo.tassinari@gmail.com>
2018-04-11 13:55:01 +0200

    first commit

---
+++ app/src/main/java/com/edotassi/amazmodcompanionservice/Constants.java
@@ -0,0 +1,17 @@
+package com.edotassi.amazmodcompanionservice;
+
+/**
+ * Created by edoardotassinari on 04/04/18.
+ */
+
+public class Constants {
+
+    public static final String TAG = "Amazmod";
+    public static final String TAG_NIGHTSCOUT_PAGE = "Amazmod:Nighscout";
+
+    public static final String PACKAGE_NAME = "com.edotassi.amazmodcompanionservice";
+
+    public static final String TRANSPORTER_MODULE = "com.edotassi.amazmod";
+
+    public static final String ACTION_NIGHTSCOUT_SYNC = "nightscout_sync";
+}
